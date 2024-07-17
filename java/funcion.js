03ed640d8da82b430b4a0fae97089287d1bb548f
Marlon Andrés Román Ríos <marlon210499@gmail.com>
2024-07-16 22:12:14 -0500

    resultado celsius

--- java/funcion.js
+++ java/funcion.js
@@ -10,10 +10,12 @@ function convertir(){
     if(aFahrenheit.checked){
         temp=Number(textBox.value);
         temp=temp * 9 / 5 + 32;
-        result.textContent = temp + "°F";
+        result.textContent = temp.toFixed(1) + "°F";
     }
     else if(aCelsius.checked){
-
+        temp=Number(textBox.value);
+        temp=(temp - 32) / (5 / 9);
+        result.textContent = temp.toFixed(1) + "°C";
     }
     else{
 
